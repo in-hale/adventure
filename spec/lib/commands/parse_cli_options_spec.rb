49bfd5bae247daa67bdc7ca636fc0da7100f9cc5
in-hale <mkrvch.yury@gmail.com>
2021-06-23 00:20:46 +0300

    add parse_cli_options_spec.rb absence comment

---
+++ spec/lib/commands/parse_cli_options_spec.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+# Tested in terms of run/adventure_spec.rb
--- spec/lib/validators/story_completable_spec.rb
+++ spec/lib/validators/story_completable_spec.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-# Tested in terms of initialize_scenes_from_file_spec.rb
+# Tested in terms of commands/initialize_scenes_from_file_spec.rb
